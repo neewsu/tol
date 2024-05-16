5029c134cf70f5e1e55ca1d8d57dff131075a950
Annie <huahuaannie@huahuaAnniedeMacBook-Air.local>
2024-05-16 09:32:28 +0800

    Use Suspense to make a delightful user experience.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,7 +20,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -34,7 +34,6 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-  noStore()
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -47,6 +46,7 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
@@ -55,6 +55,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate

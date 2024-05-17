93bba3451796f0e76aef6944a2cdb2ba88263f84
Annie <huahuaannie@huahuaAnniedeMacBook-Air.local>
2024-05-17 09:17:53 +0800

    Add metadata to app for the best SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css'
 import {inter} from "@/app/ui/fonts";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Tol Dashboard",
+    default: "Tol Dashboard"
+  },
+  description: 'The official Tol Acme Dashboard, built with AppRouter',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh')
+}
 
 export default function RootLayout({
   children,
